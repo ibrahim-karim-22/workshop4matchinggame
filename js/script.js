ec55973394d107a218fcc897aae4570a142dbd3f
karim <hema_thechamp@yahoo.com>
2024-02-20 21:43:26 -0500

    done

--- js/script.js
+++ js/script.js
@@ -83,8 +83,6 @@ function startGame(difficulty) {
 
 function resetGame() {
   gameHasStarted = false;
-  // numberOfFaces = 5;
-  //theAttDisplay.textContent = '1';
   currentLevel = 2;
   theLeftSide.innerHTML = "";
   theRightSide.innerHTML = "";
@@ -96,7 +94,6 @@ function mainMenuPress() {
   resetGame();
   theGameArea.style.display = "none";
   theStartBtnContainer.style.display = "block";
-  //theSettingsBtnContainer.style.display = "block";
   theInfoBtnContainer.style.display = "block";
   theButtonsBg.style.display = "block";
   theMenuBtnContainer.style.display = "none";
@@ -109,7 +106,6 @@ function mainMenuPress() {
   theMediumBtn.style.display = "block";
   theHardBtn.style.display = "block";
   theGoodLuckBtn.style.display = "block";
-  //  generateFaces();
 }
 function infoPage() {
   theGameArea.style.display = "none";
@@ -125,14 +121,14 @@ function infoPage() {
   theHardBtn.style.display = "none";
   theGoodLuckBtn.style.display = "none";
   theInfoBtnContainer.style.display = "none";
-   theInfoSentence.style.display = "block";
+  theInfoSentence.style.display = "block";
 }
 
 function generateFaces(difficulty) {
   if (!gameHasStarted) {
     return;
   }
-  
+
   switch (difficulty) {
     case "easy":
       numberOfFaces = 2;
@@ -141,21 +137,21 @@ function generateFaces(difficulty) {
     case "medium":
       numberOfFaces = 5;
       imageUrls = ["https://i.giphy.com/pUjFF6SVlR5OeKEqCY.webp", "https://i.giphy.com/dQx8nzM8Z1EJXbBqk3.webp", "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWxrdWllZ25oYjhzcnRubW56Y3dreWQ2cHI2YjJ4aDM0emF0aXYyaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/p94nK1krMpGIkJYYJv/giphy.gif",
-      "https://i.giphy.com/d2Sor6kd4bug3J3Srk.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp"]
+        "https://i.giphy.com/d2Sor6kd4bug3J3Srk.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp"]
       break;
     case "hard":
       numberOfFaces = 8;
-      imageUrls = ["https://i.giphy.com/T6oln8tuKfrrE7Wh47.webp", 
-      "https://i.giphy.com/l46Cpss7mfcEftf56.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp","https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif",
-      "https://i.giphy.com/llxTZC250AVby0Xdan.webp", "https://i.giphy.com/C0Rvn1BlqQQgM.webp", "https://i.giphy.com/llxTZC250AVby0Xdan.webp", 
-      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif"]
+      imageUrls = ["https://i.giphy.com/T6oln8tuKfrrE7Wh47.webp",
+        "https://i.giphy.com/l46Cpss7mfcEftf56.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp", "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif",
+        "https://i.giphy.com/llxTZC250AVby0Xdan.webp", "https://i.giphy.com/C0Rvn1BlqQQgM.webp", "https://i.giphy.com/llxTZC250AVby0Xdan.webp",
+        "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif"]
       break;
     case "goodLuck":
       numberOfFaces = 10;
-      imageUrls = ["https://i.giphy.com/JLGhqNoi9xsnS.webp" ,"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2tjZnhtYWZxbjhhc3kycWk5YnBiYW9iM2lkOXoweWUxN2hwZGRzZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/SHgA5d6Aoqp7OJXdCC/giphy.gif",
-                  "https://i.giphy.com/T6oln8tuKfrrE7Wh47.webp", "https://i.giphy.com/pUjFF6SVlR5OeKEqCY.webp", "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWxrdWllZ25oYjhzcnRubW56Y3dreWQ2cHI2YjJ4aDM0emF0aXYyaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/p94nK1krMpGIkJYYJv/giphy.gif",
-                  "https://i.giphy.com/l46Cpss7mfcEftf56.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp", "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif",
-                  "https://i.giphy.com/dQx8nzM8Z1EJXbBqk3.webp", "https://i.giphy.com/llxTZC250AVby0Xdan.webp", "https://i.giphy.com/C0Rvn1BlqQQgM.webp", "https://i.giphy.com/d2Sor6kd4bug3J3Srk.webp",]
+      imageUrls = ["https://i.giphy.com/JLGhqNoi9xsnS.webp", "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2tjZnhtYWZxbjhhc3kycWk5YnBiYW9iM2lkOXoweWUxN2hwZGRzZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/SHgA5d6Aoqp7OJXdCC/giphy.gif",
+        "https://i.giphy.com/T6oln8tuKfrrE7Wh47.webp", "https://i.giphy.com/pUjFF6SVlR5OeKEqCY.webp", "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWxrdWllZ25oYjhzcnRubW56Y3dreWQ2cHI2YjJ4aDM0emF0aXYyaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/p94nK1krMpGIkJYYJv/giphy.gif",
+        "https://i.giphy.com/l46Cpss7mfcEftf56.webp", "https://i.giphy.com/2VVvpkyyym5FOrHQG3.webp", "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdxb2RrcTB6bXdmeWNwdHBmZjhhOWd2eDFiNDhwdWllZHRyN3BqbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/AoFwZ9cx7FcWY/giphy.gif",
+        "https://i.giphy.com/dQx8nzM8Z1EJXbBqk3.webp", "https://i.giphy.com/llxTZC250AVby0Xdan.webp", "https://i.giphy.com/C0Rvn1BlqQQgM.webp", "https://i.giphy.com/d2Sor6kd4bug3J3Srk.webp",]
       break;
     default:
       console.error('invalid');
@@ -164,11 +160,11 @@ function generateFaces(difficulty) {
   for (let i = 0; i < imageUrls.length; i++) {
     console.log("Image URL " + i + ": " + imageUrls[i]);
   }
-    // Clear the left and right sides before generating new faces
-    numberOfFaces *= currentLevel;
-    theLeftSide.innerHTML = "";
-    theRightSide.innerHTML = "";
-    console.log('left: ' + theLeftSide.innerHTML + 'right: ' + theRightSide.innerHTML);
+  // Clear the left and right sides before generating new faces
+  numberOfFaces *= currentLevel;
+  theLeftSide.innerHTML = "";
+  theRightSide.innerHTML = "";
+  console.log('left: ' + theLeftSide.innerHTML + 'right: ' + theRightSide.innerHTML);
   for (let i = 0; i < numberOfFaces; i++) {
     face = document.createElement("img");
     face.src = imageUrls[i % imageUrls.length];
@@ -180,62 +176,61 @@ function generateFaces(difficulty) {
     face.title = "Image ";
     theLeftSide.appendChild(face);
   }
-    extraImage = document.createElement("img");
-    extraImageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-    extraImage.src = extraImageUrl;
-    extraImage.style.width = "77px";
-    randomExtraTop = Math.floor(Math.random() * 400) + 1;
-    randomExtraLeft = Math.floor(Math.random() * 400) + 1;
-    extraImage.style.top = randomExtraTop + "px";
-    extraImage.style.left = randomExtraLeft + "px";
-    theLeftSide.appendChild(extraImage);
-    
-    leftImgCopy = theLeftSide.cloneNode(true);
-   leftImgCopy.removeChild(leftImgCopy.lastChild); 
-    theRightSide.appendChild(leftImgCopy);
+  extraImage = document.createElement("img");
+  extraImageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+  extraImage.src = extraImageUrl;
+  extraImage.style.width = "77px";
+  randomExtraTop = Math.floor(Math.random() * 400) + 1;
+  randomExtraLeft = Math.floor(Math.random() * 400) + 1;
+  extraImage.style.top = randomExtraTop + "px";
+  extraImage.style.left = randomExtraLeft + "px";
+  theLeftSide.appendChild(extraImage);
 
-      extraImageClickListener = function(event) {
-        nextLevel(event, difficulty);
-    };
-    if (previousExtraImage) {
-        previousExtraImage.removeEventListener("click", extraImageClickListener);
-    }
-    extraImage.addEventListener("click", extraImageClickListener);
-    previousExtraImage = extraImage;
-    
+  leftImgCopy = theLeftSide.cloneNode(true);
+  leftImgCopy.removeChild(leftImgCopy.lastChild);
+  theRightSide.appendChild(leftImgCopy);
+
+  extraImageClickListener = function (event) {
+    nextLevel(event, difficulty);
+  };
+  if (previousExtraImage) {
+    previousExtraImage.removeEventListener("click", extraImageClickListener);
+  }
+  extraImage.addEventListener("click", extraImageClickListener);
+  previousExtraImage = extraImage;
+
 }
 
 function nextLevel(event, difficulty) {
   if (!gameHasStarted) {
-      return;
+    return;
   }
   event.stopPropagation();
-  currentLevel++; // Increment the current level
+  currentLevel++;
   generateFaces(difficulty);
   countAttempts();
   timeLeft += 5;
-  timerSpan.style.color = 'green'; // Change timer color to green
- timerSpan.textContent = timeLeft + '+++++';
-timerSpan.style.fontSize = '40px';
-timerSpan.style.position = 'absolute';
-timerSpan.style.top = '0';
-setTimeout(function() {
-  timeLeft.style.left = '44px'// Change timer color back to blue after 1 second
-}, 2000);
-
-  setTimeout(function() {
-    timerSpan.style.color = 'blue'; // Change timer color back to blue after 1 second
+  timerSpan.style.color = 'green';
+  timerSpan.textContent = timeLeft + '+++++';
+  timerSpan.style.fontSize = '40px';
+  timerSpan.style.position = 'absolute';
+  timerSpan.style.top = '0';
+  setTimeout(function () {
+    timeLeft.style.left = '44px'
   }, 2000);
-  setTimeout(function() {
-    theAttDisplay.style.color = 'green'; 
+
+  setTimeout(function () {
+    timerSpan.style.color = 'blue';
+  }, 2000);
+  setTimeout(function () {
+    theAttDisplay.style.color = 'green';
   }, 1000);
 
-  // Change attempts display color back to white after 2 seconds
-  setTimeout(function() {
-    theAttDisplay.style.color = 'white'; 
+
+  setTimeout(function () {
+    theAttDisplay.style.color = 'white';
   }, 2000);
 }
-
 
 function countAttempts() {
   if (!gameHasStarted) {
@@ -261,17 +256,17 @@ function gameOver() {
   theGameOverScreen.style.display = "block";
   if (theAttDisplay.innerHTML === '1') {
     theGameOverMessage.innerHTML = userName + " Try Again!"
-  }else {
-  theGameOverMessage.innerHTML = userName +  " You Reached " + theAttDisplay.textContent;
-}
+  } else {
+    theGameOverMessage.innerHTML = userName + " You Reached " + theAttDisplay.textContent;
+  }
   gameHasStarted = false;
   theMenuBtnContainer.addEventListener("click", mainMenuPress);
   document.body.removeEventListener("click", gameOver);
-  if(theLeftSide.lastChild){
-  theLeftSide.lastChild.removeEventListener("click", nextLevel);
-}}
+  if (theLeftSide.lastChild) {
+    theLeftSide.lastChild.removeEventListener("click", nextLevel);
+  }
+}
 
-///timer///
 function updateTimer() {
   if (timeLeft > 0) {
     timeLeft = timeLeft - 1;
@@ -286,17 +281,17 @@ function updateTimer() {
 }
 
 document.querySelector("#easyButton").addEventListener("click", function () {
-    startGame("easy");
-  });
-  document.querySelector("#mediumButton").addEventListener("click", function () {
-    startGame("medium");
-  });
-  document.querySelector("#hardButton").addEventListener("click", function () {
-    startGame("hard");
-  });
-  document.querySelector("#goodLuckButton").addEventListener("click", function () {
-    startGame("goodLuck");
-  });
-  document.querySelector("#gameArea").addEventListener("click", gameOver);
-  document.querySelector("#menuButton").addEventListener("click", mainMenuPress);
-  document.querySelector('#infoButton').addEventListener("click", infoPage)
+  startGame("easy");
+});
+document.querySelector("#mediumButton").addEventListener("click", function () {
+  startGame("medium");
+});
+document.querySelector("#hardButton").addEventListener("click", function () {
+  startGame("hard");
+});
+document.querySelector("#goodLuckButton").addEventListener("click", function () {
+  startGame("goodLuck");
+});
+document.querySelector("#gameArea").addEventListener("click", gameOver);
+document.querySelector("#menuButton").addEventListener("click", mainMenuPress);
+document.querySelector('#infoButton').addEventListener("click", infoPage)
